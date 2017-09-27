9b09512f1c836f2a2b7b8facc322a787aa01b472
Vladimir Valter <vvalter@rbc.ru>
2017-09-27 17:55:33 +0300

    Initial Commit

---
+++ vision-ML-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vision-ML-test
+//
+//  Created by Vladimir Valter on 27/09/2017.
+//  Copyright © 2017 Vladimir Valter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
